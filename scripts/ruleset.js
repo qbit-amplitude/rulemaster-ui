3afa93114985e403cc9627aa4feae7467a025c0c
Divya Meharwade <dmeharwa@akamai.com>
2017-11-04 14:32:27 +0530

    response

--- scripts/ruleset.js
+++ scripts/ruleset.js
@@ -25,7 +25,9 @@ $(document).ready(function(){
         type: "POST",
         url: "http://52.77.80.70/api/v1/"+rulesetname+"/definition",
         data: JSON.stringify(obj),
-        success: function(json) { console.log(json); alert(json); console.log(json)}
+        success: function(json) {
+            console.log(JSON.parse(json))
+        }
 
         });
 
